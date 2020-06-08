99654c43072e71776ce70f854f71e7bfe22e9d44
Botezatu-Andrei <66585353+Botezatu-Andrei@users.noreply.github.com>
2020-06-08 08:28:18 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Navbar } from "./Navbar/Navbar";
+import { Banner } from "./Banner/Banner";
+import { Menu } from "./Menu/Menu";
+import { FoodDialog } from "./FoodDialog/FoodDialog";
+import { GlobalStyle } from "./Styles/GlobalStyle";
+import { Order } from "./Order/Order";
+import { useOpenFood } from "./Hooks/useOpenFood";
+import { useOrders } from "./Hooks/useOrders";
+import { useTitle } from "./Hooks/useTitle";
+import { searchBox} from "./searchBox/searchBox";
+import { Checkbox} from "./Checkbox/Checkbox";
+
+
+
+function App() {
+  const openFood = useOpenFood();
+  const orders = useOrders();
+  useTitle({ ...openFood, ...orders });
+
+  return (
+    <>
+      <GlobalStyle />
+      <FoodDialog {...openFood} {...orders} />
+      <Navbar />
+      <Order {...orders} {...openFood} />
+      <Banner />
+      <Menu {...openFood} />
+      
+    </>
+  );
+}
+
+export default App;
+
+
